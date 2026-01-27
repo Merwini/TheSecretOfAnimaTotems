9c6d9aca699c4f4b2c46a59d6e3948178f2ea160
Merwini <86023141+Merwini@users.noreply.github.com>
2026-01-26 20:00:45 -0500

    CompAnimusTotem

---
+++ Source/TheSecretOfAnimaTotems/Comps/CompAnimusTotem.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+using tsoa.core;
+
+namespace tsoa.totems;
+
+public abstract class CompAnimusTotem : ThingComp
+{
+    private CompGroupedFacility compGF;
+    public CompGroupedFacility CompGF
+    {
+        get
+        {
+            if (compGF == null)
+            {
+                compGF = parent.GetComp<CompGroupedFacility>();
+            }
+            return compGF;
+        }
+    }
+
+    private CompSpawnSubplant compSP;
+    public CompSpawnSubplant CompSpawnSubplant
+    {
+        get
+        {
+            if (compSP == null)
+            {
+                List<Thing> linkedThings = CompGF.LinkedThings;
+                if (linkedThings.NullOrEmpty())
+                    return null;
+
+                for (int i = 0; i < linkedThings.Count; i++)
+                {
+                    CompSpawnSubplant comp = linkedThings[i].TryGetComp<CompSpawnSubplant>();
+                    if (comp != null)
+                    {
+                        compSP = comp;
+                        break;
+                    }
+                }
+            }
+            return compSP;
+        }
+    }
+
+    public abstract void DoTotemEffect();
+}
---
+++ Source/TheSecretOfAnimaTotems/Comps/CompProperties_AnimusTotem.cs
@@ -0,0 +1,20 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tsoa.core;
+using Verse;
+
+namespace tsoa.totems;
+
+public class CompProperties_AnimusTotem : CompProperties
+{
+    public float progressConsumption = 0.5f;
+
+    public CompProperties_AnimusTotem()
+    {
+        this.compClass = typeof(CompAnimusTotem);
+    }
+}
